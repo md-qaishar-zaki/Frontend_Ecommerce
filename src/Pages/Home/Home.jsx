fbf875126d1d719704fb07e644f72563f6b28fa6
Md Qaishar Zaki <109577871+md-qaishar-zaki@users.noreply.github.com>
2024-10-22 13:17:00 +0000

    .

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return <h1 className='h-screen flex justify-center items-center bg-red-500 p-4'>Welcome to Home Page</h1>;
+};
+
+export default Home;

40072442614ad0434614367d100353258a47ff52
Md Qaishar Zaki <qaisharzaki@gmail.com>
2024-10-27 00:59:32 +0530

    .

--- src/Pages/Product_Details/Project_Details.jsx
+++ src/Pages/Product_Details/Project_Details.jsx
@@ -55,26 +55,48 @@ export default function Project_Details() {
             <section className="py-5 pt-2">
                 <div className="container mx-auto">
                     <div className="flex flex-wrap ProductDetails py-5">
-                        <aside className="w-full lg:w-1/2 px-4 mb-4 lg:mb-0">
-                            <div className="ProductIMG w-full h-auto rounded-lg shadow-md mb-4">
-                                <img
-                                    id="mainImage"
-                                    src={product.product.photoproduct?.[0]?.photo_path}
-                                    alt={product.product.title}
-                                    onClick={openModal}
-                                    className="cursor-pointer"
-                                />
-                            </div>
-                            <div className="flex gap-4 py-4 justify-center overflow-x-auto ProductIMGList">
+                        <aside className="ProductImgSticky w-full lg:w-1/2 px-4 mb-4 lg:mb-0 flex lg:flex-row">
+                            {/* Thumbnails on the left side */}
+                            <div className="flex flex-col gap-4 py-4 justify-start overflow-y-auto ProductIMGList">
                                 {product.product.photoproduct?.map((photo, index) => (
                                     <img
                                         key={index}
                                         src={photo.photo_path}
                                         alt={`${product.product.title} - ${index + 1}`}
-                                        className={`w-16 h-16 sm:w-20 sm:h-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300 ${true ? 'opacity-100' : ''}`}
+                                        className="w-16 h-16 sm:w-20 sm:h-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
                                         onClick={() => changeImage(photo.photo_path)}
                                     />
                                 ))}
+                            </div>
+
+                            {/* Main Image and Buttons on the right side */}
+                            <div className="d-flex">
+                                <div className="ProductIMG w-full rounded-lg shadow-md ml-4">
+                                    <img
+                                        id="mainImage"
+                                        src={product.product.photoproduct?.[0]?.photo_path}
+                                        alt={product.product.title}
+                                        onClick={openModal}
+                                        className="cursor-pointer"
+                                    />
+                                </div>
+                                <ul className="flex flex-wrap mt-4">
+                                    <li className="w-1/2 p-2">
+                                        <button className="flex items-center justify-center w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded shadow transition duration-300">
+                                            <svg className="mr-2" width="16" height="16" viewBox="0 0 16 15" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M15.32 2.405H4.887C3 2.405 2.46.805 2.46.805L2.257.21C2.208.085 2.083 0 1.946 0H.336C.1 0-.064.24.024.46l.644 1.945L3.11 9.767c.047.137.175.23.32.23h8.418l-.493 1.958H3.768l.002.003c-.017 0-.033-.003-.05-.003-1.06 0-1.92.86-1.92 1.92s.86 1.92 1.92 1.92c.99 0 1.805-.75 1.91-1.712l5.55.076c.12.922.91 1.636 1.867 1.636 1.04 0 1.885-.844 1.885-1.885 0-.866-.584-1.593-1.38-1.814l2.423-8.832c.12-.433-.206-.86-.655-.86" fill="currentColor"></path>
+                                            </svg>
+                                            Add to Cart
+                                        </button>
+                                    </li>
+                                    <li className="w-1/2 p-2 flex">
+                                        <form className="w-full">
+                                            <button className="w-full py-2 px-4 bg-green-500 hover:bg-green-600 text-white font-semibold rounded shadow transition duration-300" type="button">
+                                                Buy Now
+                                            </button>
+                                        </form>
+                                    </li>
+                                </ul>
                             </div>
                         </aside>
                         <main className="w-full lg:w-1/2 px-4">
